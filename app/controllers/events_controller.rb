d90066f0de0fe8fa66fd6820cb3f24ba5db9142b
Wes <wes.coffay@gmail.com>
2017-03-10 15:33:08 -0500

    added controllers for events and students

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,56 @@
+class EventsController < ApplicationController
+
+  def index
+    @events = Event.all
+    respond_to do |format|
+      format.html {render :index}
+      format.json {render json: @events}
+    end
+  end
+
+  def show
+    @event = Event.find(params[:id])
+    respond_to do |format|
+      format.html {render :show}
+      format.json {render json: @event}
+    end
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    respond_to do |format|
+      if @event.save
+        format.html {redirect_to @event, notice "Event added"}
+        format.json {render json: @event, status: :created, location @event}
+      else
+        format.html {render :new}
+        format.json {render json: @event.errors, status: :unprocessable_entity}
+      end
+    end
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    @event.update(event_params)
+    redirect_to @event
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to events_path
+  end
+
+  private
+  def event_params
+    params.require(:event).permit(:date, :time, :name, :location, :organizer, :event_url)
+  end
+end
---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,56 @@
+class StudentsController < ApplicationController
+
+    def index
+      @students = Student.all
+      respond_to do |format|
+        format.html {render :index}
+        format.json {render json: @students}
+      end
+    end
+
+    def show
+      @student = Student.find(params[:id])
+      respond_to do |format|
+        format.html {render :show}
+        format.json {render json: @student}
+      end
+    end
+
+    def new
+      @student = Student.new
+    end
+
+    def create
+      @student = Student.new(student_params)
+      respond_to do |format|
+        if @student.save
+          format.html {redirect_to @student, notice "Student added"}
+          format.json {render json: @student, status: :created, location @student}
+        else
+          format.html {render :new}
+          format.json {render json: @student.errors, status: :unprocessable_entity}
+        end
+      end
+    end
+
+    def edit
+      @student = Student.find(params[:id])
+    end
+
+    def update
+      @student = Student.find(params[:id])
+      @student.update(student_params)
+      redirect_to @student
+    end
+
+    def destroy
+      @student = Student.find(params[:id])
+      @student.destroy
+      redirect_to students_path
+    end
+
+    private
+    def student_params
+      params.require(:student).permit(:name, :email, :linkedin_url, :github_url, :bio)
+    end
+end
